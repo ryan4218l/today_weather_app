15fe5229545b0554cff318562e89371964a4bd81
Ryan Lio <rayanljy@gmail.com>
2026-03-16 23:14:02 +0800

    base template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import bgLight from "./assets/bg-light.png";
+import bgDark from "./assets/bg-dark.png";
+
+import { Box, IconButton, Stack, TextField, Tooltip } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      sx={{
+        backgroundImage: `url(${bgLight})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "20px",
+      }}>
+      <Stack direction="row" spacing={2} sx={{ width: "50%" }}>
+        <Box
+          sx={{
+            height: "50px",
+            width: "50%",
+            background: "rgba(255, 255, 255, 0.18)",
+            borderRadius: "16px",
+            boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+            backdropFilter: "blur(5px)",
+            WebkitBackdropFilter: "blur(5px)",
+            border: "1px solid rgba(255, 255, 255, 0.3)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "relative",
+          }}>
+          <Box sx={{ position: "absolute", top: 4, left: 20, fontSize: "12px", color: "grey" }}>Country</Box>
+          <TextField
+            variant="outlined"
+            sx={{
+              height: "100%",
+              width: "100%",
+              background: "none",
+              borderRadius: "16px",
+              border: "none",
+              "& .MuiOutlinedInput-notchedOutline": {
+                border: "none",
+              },
+            }}
+          />
+        </Box>
+        <Box
+          sx={{
+            height: "50px",
+            width: "50%",
+            background: "rgba(255, 255, 255, 0.18)",
+            borderRadius: "16px",
+            boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+            backdropFilter: "blur(5px)",
+            WebkitBackdropFilter: "blur(5px)",
+            border: "1px solid rgba(255, 255, 255, 0.3)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "relative",
+          }}>
+          <Box sx={{ position: "absolute", top: 4, left: 20, fontSize: "12px", color: "grey" }}>City</Box>
+          <TextField
+            variant="outlined"
+            sx={{
+              height: "100%",
+              width: "100%",
+              background: "none",
+              borderRadius: "16px",
+              border: "none",
+              "& .MuiOutlinedInput-notchedOutline": {
+                border: "none",
+              },
+            }}
+          />
+        </Box>
+        <Box
+          sx={{
+            height: "50px",
+            background: "rgba(165, 55,253, 1)",
+            borderRadius: "16px",
+            border: "1px solid rgba(165, 55,253, 1)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+          <Tooltip title="Search" placement="top">
+            <IconButton aria-label="search" sx={{ color: "white", borderRadius: "16px", height: "60px" }}>
+              <SearchIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      </Stack>
+      <Box
+        sx={{
+          height: "80%",
+          width: "50%",
+          background: "rgba(255, 255, 255, 0.18)",
+          borderRadius: "16px",
+          boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+          backdropFilter: "blur(5px)",
+          WebkitBackdropFilter: "blur(5px)",
+          border: "1px solid rgba(255, 255, 255, 0.3)",
+        }}>
+        Today's Weather
+      </Box>
+    </Box>
   );
 }
 
---
+++ src/constant.jsx
@@ -0,0 +1,10 @@
+export const base_url = "http://98.70.40.39:5001";
+
+export const API_KEY = "5211a61f5f133e8c37f8669fb35c7ff6";
+
+// // ------------------------------------ REGEX DECLARATION ------------------------------------
+// export const EMAIL_REGEX = /^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+// export const TABLE_NAME_REGEX = /^[a-z0-9_]+$/;
+// export const FUNCTION_NAME_REGEX = /^[a-z0-9_]+$/;
+// export const PROJECT_NAME_REGEX = /^[a-zA-Z0-9_]+$/;
+// export const ONLY_NUMBER_REGEX = /^[0-9]*$/;
